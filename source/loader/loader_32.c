319a608a1f6380d9875222ecc5520165c12a848e
KEN <liyuangang66@gmail.com>
2024-03-28 15:37:44 -0700

    format file

--- source/loader/loader_32.c
+++ source/loader/loader_32.c
@@ -10,40 +10,44 @@
  * 2. 通过0x1F2~0x1F5端口设置LBA参数, 0x1F2端口设置扇区数, 0x1F3~0x1F5端口设置LBA参数
  *    LBA参数的0~7位存放的是扇区号, 8~15位存放的是柱面号, 16~23位存放的是磁头号, 24~27位存放的是扇区数
  * 3. 通过0x1F7端口发送读取命令
-*/
-static void read_disk(int sector, int sector_count, uint8_t * buf) {
-    outb(0x1F6, (uint8_t) (0xE0));
+ */
+static void read_disk(int sector, int sector_count, uint8_t *buf)
+{
+    outb(0x1F6, (uint8_t)(0xE0));
 
-	outb(0x1F2, (uint8_t) (sector_count >> 8));
-    outb(0x1F3, (uint8_t) (sector >> 24));		// LBA参数的24~31位
-    outb(0x1F4, (uint8_t) (0));					// LBA参数的32~39位
-    outb(0x1F5, (uint8_t) (0));					// LBA参数的40~47位
+    outb(0x1F2, (uint8_t)(sector_count >> 8));
+    outb(0x1F3, (uint8_t)(sector >> 24)); // LBA参数的24~31位
+    outb(0x1F4, (uint8_t)(0));            // LBA参数的32~39位
+    outb(0x1F5, (uint8_t)(0));            // LBA参数的40~47位
 
-    outb(0x1F2, (uint8_t) (sector_count));
-	outb(0x1F3, (uint8_t) (sector));			// LBA参数的0~7位
-	outb(0x1F4, (uint8_t) (sector >> 8));		// LBA参数的8~15位
-	outb(0x1F5, (uint8_t) (sector >> 16));		// LBA参数的16~23位
+    outb(0x1F2, (uint8_t)(sector_count));
+    outb(0x1F3, (uint8_t)(sector));       // LBA参数的0~7位
+    outb(0x1F4, (uint8_t)(sector >> 8));  // LBA参数的8~15位
+    outb(0x1F5, (uint8_t)(sector >> 16)); // LBA参数的16~23位
 
-	outb(0x1F7, (uint8_t) 0x24);
+    outb(0x1F7, (uint8_t)0x24);
 
-	// 读取数据
-	uint16_t *data_buf = (uint16_t*) buf;
-	while (sector_count-- > 0) {
-		// 每次扇区读之前都要检查，等待数据就绪
-		while ((inb(0x1F7) & 0x88) != 0x8) {}
+    // 读取数据
+    uint16_t *data_buf = (uint16_t *)buf;
+    while (sector_count-- > 0)
+    {
+        // 每次扇区读之前都要检查，等待数据就绪
+        while ((inb(0x1F7) & 0x88) != 0x8)
+        {
+        }
 
-		// 读取并将数据写入到缓存中
-		for (int i = 0; i < SECTOR_SIZE / 2; i++) {
-			*data_buf++ = inw(0x1F0);
-		}
-	}
+        // 读取并将数据写入到缓存中
+        for (int i = 0; i < SECTOR_SIZE / 2; i++)
+        {
+            *data_buf++ = inw(0x1F0);
+        }
+    }
 }
 
-
-void load_kernel (void) {
-    // 读取内核镜像
+void load_kernel(void)
+{
+    // 读取内核镜像, kernel文件放到了100号扇区
+    // (使用命令dd if=kernel.elf of=$DISK1_NAME bs=512 conv=notrunc seek=100把kernel.elf文件写入到100号扇区)
     read_disk(100, 500, (uint8_t *)SYS_KERNEL_LOAD_ADDR);
-
-    
-    
+    ((void (*)(void))SYS_KERNEL_LOAD_ADDR)(); // 跳转到kernel的入口地址
 }
